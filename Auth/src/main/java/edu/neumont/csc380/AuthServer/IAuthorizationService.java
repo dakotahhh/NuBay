c91c07ba669b92604995652bc0ec0b96a67b468a
scottmcnulty <smcnulty@student.neumont.edu>
2014-05-08 21:43:56 -0600

    commit to pull

--- Auth/src/main/java/edu/neumont/csc380/AuthServer/IAuthorizationService.java
+++ Auth/src/main/java/edu/neumont/csc380/AuthServer/IAuthorizationService.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-import client.client.AuthPayload;
+import edu.neumont.csc380.AuthServer.AuthPayload;
 
 @Path("/authorize")
 public interface IAuthorizationService {
